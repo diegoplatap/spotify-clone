b7b03c3a6da06a5d904ab7499427c06c614b1836
Diego Plata <diegooplata@gmail.com>
2021-12-09 14:41:48 -0500

    Create center component

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -5,12 +5,21 @@ import {
   PlusCircleIcon,
   HeartIcon,
   RssIcon,
-} from "@heroicons/react/outline";
+} from '@heroicons/react/outline'
+import { signOut, useSession } from 'next-auth/react'
 
 function Sidebar() {
+  const { data: session, status } = useSession()
+
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 scrollbar-hide overflow-y-scroll h-screen">
       <div className="space-y-4">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <p>Log Out</p>
+        </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
@@ -47,9 +56,17 @@ function Sidebar() {
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
       </div>
     </div>
-  );
+  )
 }
 
-export default Sidebar;
+export default Sidebar
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
-import Sidebar from "../components/Sidebar.jsx";
+import Center from '../components/Center.jsx'
+import Sidebar from '../components/Sidebar.jsx'
 
 export default function Home() {
   return (
-    <div className="bg-black h-screen overflow-hidden">
-      <main>
+    <div className='bg-black h-screen overflow-hidden'>
+      <main className='flex'>
         <Sidebar />
-        {/* {Center} */}
+        <Center />
       </main>
       <div>{/* {Player} */}</div>
     </div>
-  );
+  )
 }
