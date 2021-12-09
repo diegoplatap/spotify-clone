b7706f7a0f30195b53550b9ee462e5b57a00bb3d
Diego Plata <diegooplata@gmail.com>
2021-12-09 15:24:52 -0500

    fetch playlists from spotify user auth

--- components/Center.jsx
+++ components/Center.jsx
@@ -1,8 +1,27 @@
 import { ChevronDownIcon } from '@heroicons/react/outline'
 import { useSession } from 'next-auth/react'
+import { useEffect, useState } from 'react'
+import { shuffle } from 'lodash'
+
+const colors = [
+  'from-indigo-500',
+  'from-blue-500',
+  'from-green-500',
+  'from-red-500',
+  'from-yellow-500',
+  'from-pink-500',
+  'from-purple-500',
+]
 
 function Center() {
   const { data: session } = useSession()
+
+  const [color, setColor] = useState(null)
+
+  useEffect(() => {
+    setColor(shuffle(colors).pop())
+  }, [])
+
   return (
     <div className="flex-grow ">
       <header className="absolute top-5 right-8">
@@ -17,7 +36,7 @@ function Center() {
         </div>
       </header>
       <section
-        className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white padding-8`}
+        className={`flex items-end space-x-7 bg-gradient-to-b to- ${color} from-red-500 h-80 text-white padding-8`}
       >
         <h1>hello</h1>
       </section>
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -7,9 +7,23 @@ import {
   RssIcon,
 } from '@heroicons/react/outline'
 import { signOut, useSession } from 'next-auth/react'
+import { useEffect, useState } from 'react'
+import useSpotify from '../hooks/useSpotify'
 
 function Sidebar() {
+  const spotifyApi = useSpotify()
   const { data: session, status } = useSession()
+  const [playlists, setPlaylists] = useState([])
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data) => {
+        setPlaylists(data.body.items)
+      })
+    }
+  }, [session, spotifyApi])
+
+  console.log(playlists)
 
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900 scrollbar-hide overflow-y-scroll h-screen">
@@ -43,27 +57,14 @@ function Sidebar() {
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <RssIcon className="h-5 w-5" />
-          <p>Your episodes</p>
+          <p>Your Episodes</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
-        {/* {playlist} */}
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        {playlists.map((playlist) => (
+          <p key={playlists.id} className="cursor-pointer hover:text-white">
+            {playlist.name}
+          </p>
+        ))}
       </div>
     </div>
   )
