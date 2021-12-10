3de18f8f1abf2cc881dcffd3d64d839af691184c
Diego Plata <diegooplata@gmail.com>
2021-12-09 20:27:04 -0500

    Create songs component

--- components/Songs.jsx
+++ components/Songs.jsx
@@ -1,5 +1,15 @@
+import { useRecoilValue } from 'recoil'
+import { playlistState } from '../atoms/playlistAtom'
+
 function Songs() {
-  return <div></div>
+  const playlist = useRecoilValue(playlistState)
+  return (
+    <div className="text-white">
+      {playlist?.tracks.items.map((track) => (
+        <div>{track?.track.name}</div>
+      ))}
+    </div>
+  )
 }
 
 export default Songs
