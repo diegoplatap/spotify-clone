f977b394459ede2017de137dd7f4733a0e43cb8e
Diego Plata <diegooplata@gmail.com>
2021-12-10 13:36:37 -0500

    Upgrade player component

--- components/Player.jsx
+++ components/Player.jsx
@@ -4,6 +4,18 @@ import { useRecoilState } from 'recoil'
 import { isPlayingState } from '../atoms/songAtom'
 import useSongInfo from '../hooks/useSongInfo'
 import useSpotify from '../hooks/useSpotify'
+import {
+  FastForwardIcon,
+  ReplyIcon,
+  SwitchHorizontalIcon,
+  VolumeUpIcon as VolumeDownIcon,
+} from '@heroicons/react/outline'
+import {
+  RewindIcon,
+  PlayIcon,
+  PauseIcon,
+  VolumeUpIcon,
+} from '@heroicons/react/solid'
 
 function Player() {
   const spotifyApi = useSpotify()
@@ -34,9 +46,22 @@ function Player() {
     }
   }, [currentTrackId, spotifyApi, session])
 
+  const handlePlayPause = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((data) => {
+      if (data.body.is_playing) {
+        spotifyApi.pause()
+        setIsPlaying(false)
+      } else {
+        spotifyApi.play()
+        setIsPlaying(true)
+      }
+    })
+  }
+
   return (
-    <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white">
-      <div>
+    <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
+      {/* {left} */}
+      <div className="flex items-center space-x-4 ">
         <img
           className="hidden md:inline h-10 w-10"
           src={songInfo?.album.images?.[0]?.url}
@@ -46,6 +71,30 @@ function Player() {
           <h3>{songInfo?.name}</h3>
           <p>{songInfo?.artists?.[0]?.name}</p>
         </div>
+      </div>
+      {/* {center} */}
+      <div className="flex items-center justify-evenly">
+        <SwitchHorizontalIcon className="button" />
+        <RewindIcon className="button" />
+        {isPlaying ? (
+          <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
+        ) : (
+          <PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
+        )}
+        <FastForwardIcon className="button" />
+        <ReplyIcon className="button" />
+      </div>
+      {/* {right} */}
+      <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
+        <VolumeDownIcon className="button" />
+        <input
+          className="w-14 md:w-28"
+          type="range"
+          value=""
+          min={0}
+          max={100}
+        />
+        <VolumeUpIcon className="button" />
       </div>
     </div>
   )
